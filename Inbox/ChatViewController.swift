bb5e8d480637175807a39a3b5fa06f7373337166
Mikael <miketekle14@gmail.com>
2016-11-22 09:17:38 -0800

    set up tableview controller to display fake data via Message class

--- Inbox/ChatViewController.swift
+++ Inbox/ChatViewController.swift
@@ -11,17 +11,35 @@ import UIKit
 class ChatViewController: UIViewController {
     
     @IBOutlet weak var tableView : UITableView!
-
+    fileprivate var messages = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        
+        for i in 0...10 {
+            let message = Message()
+            message.text = String(i)
+            messages.append(message)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ChatViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let message = messages[indexPath.row]
+        cell.textLabel?.text = message.text
+        return cell
+    }
+    
 }
 
