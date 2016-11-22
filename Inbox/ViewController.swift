677e4d052b499240917d969b37d99ae717a92bee
Mikael <miketekle14@gmail.com>
2016-11-21 17:47:54 -0800

    Initial Commit

---
+++ Inbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 11/21/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
