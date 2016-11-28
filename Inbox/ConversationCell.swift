fc72e151173aada93dca579d1e877da84e338167
Mikael <miketekle14@gmail.com>
2016-11-28 10:21:20 -0800

    created subclass for displaying initiated conversation

---
+++ Inbox/ConversationCell.swift
@@ -0,0 +1,15 @@
+//
+//  ConversationCell.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 11/28/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class ConversationCell: UITableViewCell {
+
+
+
+}
