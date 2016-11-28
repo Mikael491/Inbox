7b94758f612c6bca372ec0465aa61bcb904d16f0
Mikael <miketekle14@gmail.com>
2016-11-28 13:40:09 -0800

    created View Controller for displaying all available conversations

---
+++ Inbox/AllConversationsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AllConversationsViewController.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 11/28/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AllConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
