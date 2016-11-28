f0b510f052ae4937c2c5b7012da991197dc5e62c
Mikael <miketekle14@gmail.com>
2016-11-28 13:40:37 -0800

    created Cell for displaying conversation with labels for name, message preview and date

--- Inbox/ConversationCell.swift
+++ Inbox/ConversationCell.swift
@@ -12,12 +12,29 @@ class ConversationCell: UITableViewCell {
     
     let nameLabel = UILabel()
     let messageLabel = UILabel()
-    let detaLabel = UILabel()
+    let dateLabel = UILabel()
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        nameLabel.font = UIFont.systemFont(ofSize: 18, weight: UIFontWeightBold)
+        messageLabel.textColor = UIColor.gray
+        dateLabel.textColor = UIColor.gray
+        let labels = [nameLabel, messageLabel, dateLabel]
+        for label in labels {
+            label.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(label)
+        }
         
+        let labelConstraints = [
+            nameLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            messageLabel.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+            messageLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            dateLabel.firstBaselineAnchor.constraint(equalTo: nameLabel.firstBaselineAnchor)
+        ]
+        NSLayoutConstraint.activate(labelConstraints)
         
         
     }
