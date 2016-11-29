72d5c997614d1b54223de912ba12b74e63903a59
Mikael <miketekle14@gmail.com>
2016-11-29 00:22:26 -0800

    created view controller subclass for displaying newly initiated conversations

---
+++ Inbox/NewConversationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewConversationViewController.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 11/29/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class NewConversationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
