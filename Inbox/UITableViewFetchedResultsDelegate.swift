67ca110e7c9886bda8cf69110d55e3b02665a3b3
Mikael <miketekle14@gmail.com>
2016-11-29 08:16:21 -0800

    created protocol to modularize reusable code in several view controllers

---
+++ Inbox/UITableViewFetchedResultsDelegate.swift
@@ -0,0 +1,62 @@
+//
+//  UITableViewFetchedResultsDelegate.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 11/29/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+protocol UITableViewFetchedResultsDisplayer {
+    func configureCell(cell: UITableViewCell, indexPath: IndexPath)
+}
+
+class UITableViewFetchedResultsDelegate: NSObject, NSFetchedResultsControllerDelegate {
+
+    fileprivate var tableView : UITableView?
+    fileprivate var displayer : UITableViewFetchedResultsDisplayer?
+    
+    init(tableView: UITableView, displayer: UITableViewFetchedResultsDisplayer) {
+        self.tableView = tableView
+        self.displayer = displayer
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView?.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView?.insertSections(NSIndexSet.init(index: sectionIndex) as IndexSet, with: .fade)
+        case .delete:
+            tableView?.deleteSections(NSIndexSet.init(index: sectionIndex) as IndexSet, with: .fade)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView?.insertRows(at: [indexPath!], with: .fade)
+        case .update:
+            let cell = tableView?.cellForRow(at: indexPath!)
+            displayer?.configureCell(cell: cell!, indexPath: indexPath!)
+            tableView?.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            tableView?.deleteRows(at: [indexPath!], with: .fade)
+            tableView?.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView?.deleteRows(at: [indexPath!], with: .fade)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView?.endUpdates()
+    }
+    
+}
+
