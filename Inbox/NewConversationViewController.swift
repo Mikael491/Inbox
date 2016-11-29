2b15322db6d54ad088f0010308091b80081cc5bc
Mikael <miketekle14@gmail.com>
2016-11-29 00:39:51 -0800

    programmatically set up table view with constraints into NCVC

--- Inbox/NewConversationViewController.swift
+++ Inbox/NewConversationViewController.swift
@@ -11,15 +11,39 @@ import CoreData
 
 class NewConversationViewController: UIViewController {
     
+    fileprivate let tableView = UITableView(frame: .zero, style: .plain)
+    fileprivate var context : NSManagedObjectContext?
+    fileprivate var fetchedResultsController : NSFetchedResultsController<Contact>?
+    fileprivate let cellIdentifier = "ContactCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        title = "New Message"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(NewConversationViewController.cancelMessage(sender:)))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        automaticallyAdjustsScrollViewInsets = true
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        let tableViewConstraints = [
+            tableView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor)
+        ]
+        NSLayoutConstraint.activate(tableViewConstraints)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func cancelMessage(sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
     }
 
 }
