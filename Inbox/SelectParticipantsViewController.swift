0287c5ac8004031c1b067699ca07bd58fc0f76c7
Mikael <miketekle14@gmail.com>
2016-12-01 11:31:03 -0800

    created ViewController select participants for group chat

---
+++ Inbox/SelectParticipantsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SelectParticipantsViewController.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 12/1/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SelectParticipantsViewController: UIViewController {
+
+    var context : NSManagedObjectContext?
+    var conversation : Conversation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
