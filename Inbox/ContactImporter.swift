e2e5d5f5ca36db4066e184e3a9f07501700d5af6
Mikael <miketekle14@gmail.com>
2016-12-03 00:19:44 -0800

    refactored to run on new background thread

--- Inbox/ContactImporter.swift
+++ Inbox/ContactImporter.swift
@@ -27,39 +27,41 @@ class ContactImporter {
         let store = CNContactStore()
         store.requestAccess(for: .contacts, completionHandler: {
             granted, error in
-            
-            if granted {
-                do {
-                    let req = CNContactFetchRequest(keysToFetch: [
-                        CNContactGivenNameKey as CNKeyDescriptor,
-                        CNContactFamilyNameKey as CNKeyDescriptor,
-                        CNContactPhoneNumbersKey as CNKeyDescriptor
-                        ])
-                    try store.enumerateContacts(with: req, usingBlock: {
-                        cnContact, stop in
+            self.context?.perform {
+                
+                if granted {
+                    do {
+                        let req = CNContactFetchRequest(keysToFetch: [
+                            CNContactGivenNameKey as CNKeyDescriptor,
+                            CNContactFamilyNameKey as CNKeyDescriptor,
+                            CNContactPhoneNumbersKey as CNKeyDescriptor
+                            ])
+                        try store.enumerateContacts(with: req, usingBlock: {
+                            cnContact, stop in
+                            
+                            guard let contact = NSEntityDescription.insertNewObject(forEntityName: "Contact", into: self.context!) as? Contact else { return }
+                            contact.firstName = cnContact.givenName
+                            contact.lastName = cnContact.familyName
+                            contact.contactID = cnContact.identifier
+                            for cnVal in cnContact.phoneNumbers {
+                                guard let phoneNumber = NSEntityDescription.insertNewObject(forEntityName: "PhoneNumber", into: self.context!) as? PhoneNumber else { continue }
+                                phoneNumber.value = self.formatPhoneNumber(number: cnVal.value)
+                                phoneNumber.contact = contact
+                            }
+                            
+                        })
+                        try self.context?.save()
+                    } catch let error as NSError {
+                        print("There was an error importing contacts: \(error)")
+                    } catch {
                         
-                        guard let contact = NSEntityDescription.insertNewObject(forEntityName: "Contact", into: self.context!) as? Contact else { return }
-                        contact.firstName = cnContact.givenName
-                        contact.lastName = cnContact.familyName
-                        contact.contactID = cnContact.identifier
-                        let contactNumbers = NSMutableSet()
-                        for cnVal in cnContact.phoneNumbers {
-                            guard let phoneNumber = NSEntityDescription.insertNewObject(forEntityName: "PhoneNumber", into: self.context!) as? PhoneNumber else { continue }
-                            phoneNumber.value = self.formatPhoneNumber(number: cnVal.value)
-                            contactNumbers.add(phoneNumber)
-                        }
-                        contact.phoneNumbers = contactNumbers
-                        
-                    })
-                } catch let error as NSError {
-                    print("There was an error importing contacts: \(error)")
-                } catch {
-                    
+                    }
+                } else {
+                    print("Not granted access to contacts...")
                 }
-            } else {
-                print("Not granted access to contacts...")
+                
             }
-            
+
         })
         
     }
