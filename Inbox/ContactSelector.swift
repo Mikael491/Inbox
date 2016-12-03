54b22ea4c74c734aaf1fa48e3f90e6a3391fbc90
Mikael <miketekle14@gmail.com>
2016-12-03 15:38:25 -0800

    created protocol to keep track of selected contact in ContactsVC

---
+++ Inbox/ContactSelector.swift
@@ -0,0 +1,13 @@
+//
+//  ContactSelector.swift
+//  Inbox
+//
+//  Created by Mikael Teklehaimanot on 12/3/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import Foundation
+
+protocol ContactSelector {
+    func selectedContact(contact: Contact)
+}
