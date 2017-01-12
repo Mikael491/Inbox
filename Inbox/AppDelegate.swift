80beb43821e6348f1dae53acdd48ca4c34a7a005
Mikael <miketekle14@gmail.com>
2017-01-11 23:43:37 -0800

    added line to delete phone number from user defaults on app termination until logout feature implemented

--- Inbox/AppDelegate.swift
+++ Inbox/AppDelegate.swift
@@ -101,6 +101,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        UserDefaults.standard.removeObject(forKey: "phoneNumber")
     }
     
     func importContacts(_ context: NSManagedObjectContext) {
