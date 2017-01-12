1cf13855ee4cc7e3b1c041811a3158b0209e6774
Mikael <miketekle14@gmail.com>
2017-01-12 10:09:12 -0800

    added extension to fill upload protocol method with observer for contacts

--- Inbox/FirebaseModel.swift
+++ Inbox/FirebaseModel.swift
@@ -13,3 +13,27 @@ import CoreData
 protocol FirebaseModel {
     func upload(rootRef: FIRDatabaseReference, context: NSManagedObjectContext)
 }
+
+extension Contact : FirebaseModel {
+    func upload(rootRef: FIRDatabaseReference, context: NSManagedObjectContext) {
+        guard let phoneNumbers = phoneNumbers?.allObjects as? [PhoneNumber] else { return }
+        for number in phoneNumbers {
+            rootRef.child("users")
+            .queryOrdered(byChild: "phoneNumber")
+            .queryEqual(toValue: number.value)
+            .observeSingleEvent(of: .value, with: { (snapshot) in
+                guard let snap = snapshot.value as? NSDictionary else { return }
+                let uid = snap.allKeys.first as? String
+                self.storageID = uid
+                number.registered = true
+                context.perform {
+                    do {
+                        try context.save()
+                    } catch {
+                        print("Error saving context when importing phoneNumber")
+                    }
+                }
+            })
+        }
+    }
+}
