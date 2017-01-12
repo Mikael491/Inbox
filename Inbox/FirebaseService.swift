5738075a9cf49bad76c3fe89583df95c30305e2a
Mikael <miketekle14@gmail.com>
2017-01-11 23:37:11 -0800

    updated Firebase methods to updated sdk methods

--- Inbox/FirebaseService.swift
+++ Inbox/FirebaseService.swift
@@ -13,9 +13,10 @@ import CoreData
 class FirebaseService {
     
     private var context : NSManagedObjectContext
-    fileprivate let rootRef = Firebase(url: "https://inbox-6c25d.firebaseio.com/")
+    fileprivate let rootRef = FIRDatabase.database().reference(fromURL: "https://inbox-6c25d.firebaseio.com/")
+    fileprivate let authData = FIRAuth.auth()
     
-    private var currentPhoneNumber: String? {
+    fileprivate var currentPhoneNumber: String? {
         set(phoneNumber) {
             UserDefaults.standard.set(phoneNumber, forKey: "phoneNumber")
         }
@@ -27,11 +28,11 @@ class FirebaseService {
     init (context: NSManagedObjectContext) {
         self.context = context
         print("Custom Root: \(rootRef)")
-        print("Internal Root: \(rootRef?.root)")
+        print("Internal Root: \(rootRef.root)")
     }
     
     func hasAuthenticated() -> Bool {
-        return rootRef?.authData != nil
+        return UserDefaults.standard.string(forKey: "phoneNumber") != nil
     }
 }
 
@@ -46,18 +47,19 @@ extension FirebaseService : RemoteStore {
     }
     
     func signUp(phoneNumber: String, email: String, password: String, success: @escaping () -> (), error errorCallback: @escaping (String) -> ()) {
-        rootRef?.createUser(email, password: password, withValueCompletionBlock: { (error, result) in
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (result, error) in
             if (error != nil) {
-                print(error?.localizedDescription)
+                print((error?.localizedDescription)!)
                 errorCallback((error?.localizedDescription)!)
             } else {
                 let newUser = [
                     "phoneNumber" : phoneNumber
                 ]
                 print("Result: \(result)")
-                let uid = result?["uid"] as! String
-                self.rootRef?.child(byAppendingPath: "users").child(byAppendingPath: uid).setValue(newUser)
-                self.rootRef?.authUser(email, password: password, withCompletionBlock: { (error, result) in
+                let uid = result?.uid
+                self.currentPhoneNumber = phoneNumber
+                self.rootRef.child("users").child(uid!).setValue(newUser)
+                FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (result, error) in
                     if (error != nil) {
                         errorCallback((error?.localizedDescription)!)
                     } else {
